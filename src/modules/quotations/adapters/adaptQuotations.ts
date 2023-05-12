5f870db612c8f33f495f9f2dd9792d940cdb6c54
Kaluã Bentes <kaluanbentes@gmail.com>
2023-05-11 22:20:13 -0400

    refactor: improve readbility

--- src/modules/quotations/adapters/adaptQuotations.ts
+++ src/modules/quotations/adapters/adaptQuotations.ts
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 
+const getBitcoinName = (bitcoin: any) => {
+  return `${bitcoin.name} / ${bitcoin.format[0]} ${bitcoin.format[0] === 'USD' ? 'Dólar' : 'Real'}`
+}
+
 const adaptQuotations = (quotationsResponse: any) => {
   const {
     results: { currencies, bitcoin }
   } = quotationsResponse
-
-  console.log('quotations', quotationsResponse)
 
   const currenciesCollection = Object.keys(currencies)
     .filter((key) => key !== 'source')
@@ -18,9 +20,7 @@ const adaptQuotations = (quotationsResponse: any) => {
 
   const bitcoinCollection = Object.keys(bitcoin).map((key) => ({
     id: uuidv4(),
-    name: `${bitcoin[key].name} / ${bitcoin[key].format[0]} ${
-      bitcoin[key].format[0] === 'USD' ? 'Dólar' : 'Real'
-    }`,
+    name: getBitcoinName(bitcoin[key]),
     variation: bitcoin[key].variation,
     price: bitcoin[key].last
   }))
